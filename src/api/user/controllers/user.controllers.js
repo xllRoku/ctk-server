269f9ba1e1b9446d46681767ad892e1737ea27d7
Juliana Cortes Vidal <jhinlmlz@gmail.com>
2022-10-03 20:46:33 -0500

    fix error with generateJWT

--- src/api/user/controllers/user.controllers.js
+++ src/api/user/controllers/user.controllers.js
@@ -52,15 +52,21 @@ const loginUser = async (req, res) => {
       return res.status(404).json({ message: msgError.message });
     }
 
-    user.token = generateJWT(user._id);
+    const { token, expiresIn } = generateJWT(user._id);
+
     const userWithToken = await User.findOneAndUpdate(
       { email },
-      { token: user.token },
+      { token },
       { new: true }
     );
+
+    console.log(userWithToken);
+
     await user.save();
 
-    return res.status(200).json({ ok: "ok", token: userWithToken.token });
+    return res
+      .status(200)
+      .json({ ok: "ok", token: token, expiresIn: expiresIn });
   } catch (error) {
     console.log(error);
 
